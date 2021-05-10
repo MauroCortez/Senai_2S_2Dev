d9a20e9578359429584f66738d430aa20c4602c5
mauro_cortez_11@hotmail.com <mauro_cortez_11@hotmail.com>
2021-05-10 18:42:21 -0300

    Adicionado Baixa Fidelidade SPMED

--- Sprint1_BD/Projeto SP Med. Group/Scripts e Documentação/senai_spmedgroup_03_DQL.sql
+++ Sprint1_BD/Projeto SP Med. Group/Scripts e Documentação/senai_spmedgroup_03_DQL.sql
@@ -16,7 +16,7 @@ INNER JOIN Usuario
 ON Paciente.idUsuario = Usuario.idUsuario
 
 -- Medicos --
-SELECT CRM,NomeMedico,Email,Especialidade,NomeFantasia,CNPJ,RazaoSocial,Endereco FROM Medico
+SELECT CRM,NomeMedico,Email,NomeEspecialidade,NomeFantasia,CNPJ,RazaoSocial,Endereco FROM Medico
 INNER JOIN Usuario
 ON Medico.idUsuario = Usuario.idUsuario
 INNER JOIN Especialidade
@@ -25,4 +25,18 @@ INNER JOIN Clinica
 ON Medico.idClinica = Clinica.idClinica
 
 -- Especialidades -- 
-SELECT Especialidade FROM Especialidade
+SELECT NomeEspecialidade FROM Especialidade
+
+
+
+-- 11. --
+-- Teste --
+-- SELECT NomePaciente, CONVERT(CHAR,DataNascimento, 101), DATEDIFF (YEAR,DataNascimento,GETDATE()) AS "Idade Atual" FROM Paciente --
+
+
+-- Colocar a Data de Nascimento como MM/DD/YYYY --
+-- SELECT CONVERT(CHAR,DataNascimento, 101) FROM Paciente --
+
+
+-- Mostrar a Idade atual do Paciente --
+-- SELECT DATEDIFF (YEAR,DataNascimento,GETDATE()) AS "Idade Atual" FROM Paciente --
